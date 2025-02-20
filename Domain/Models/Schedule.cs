fcb940fe627069e5e0fee8c6165c36d5b079f9c3
nika1337 <nikanadaraia1337@gmail.com>
2025-02-20 17:46:14 +0400

    added schedule and schedule change request

---
+++ Domain/Models/Schedule.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Domain.Models;
+
+public class Schedule : BaseModel
+{
+    public required Guid ScheduleOfWorkerId { get; set; }
+    public required Job JobToPerform { get; set; }
+    public required DateOnly ScheduledAtDate { get; set; }
+    public required PartOfDay ScheduledAtPartOfDay { get; set; }
+    public ScheduleChangeRequest? ScheduleChangeRequest { get; set; }
+}
---
+++ Domain/Models/ScheduleChangeRequest.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Domain.Models;
+
+public class ScheduleChangeRequest : BaseModel
+{
+    public required Schedule scheduleToChange;
+    public required DateOnly NewDateUtc;
+    public required PartOfDay NewPartOfDay;
+}
