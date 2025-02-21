40855d34b90e9f8ae9c61a4c14699dfebb34f302
nika1337 <nikanadaraia1337@gmail.com>
2025-02-22 00:26:49 +0400

    added request for creating schedule

---
+++ Application/DataTransferObjects/Schedules/ScheduleCreateRequest.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Domain.Models;
+
+namespace Application.DataTransferObjects.Schedules;
+
+public record ScheduleCreateRequest
+{
+    public required Guid WorkerId { get; init; }
+    public required Guid JobId { get; init; }
+    public required DateOnly Date { get; init; }
+    public required PartOfDay PartOfDay { get; init; }
+}
