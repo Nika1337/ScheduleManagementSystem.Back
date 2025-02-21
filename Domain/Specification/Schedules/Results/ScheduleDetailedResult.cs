f0aba39697b5b43a403cfc6844cb074a90bab7a6
nika1337 <nikanadaraia1337@gmail.com>
2025-02-21 20:41:32 +0400

    added detailed result record for schedule

---
+++ Domain/Specification/Schedules/Results/ScheduleDetailedResult.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Domain.Models;
+
+namespace Domain.Specification.Schedules.Results;
+
+public record ScheduleDetailedResult
+{
+    public required Guid Id { get; init; }
+    public required Guid WorkerId { get; init; }
+    public required string JobName { get; init; }
+    public required DateOnly Date { get; init; }
+    public required PartOfDay PartOfDay { get; init; }
+}
