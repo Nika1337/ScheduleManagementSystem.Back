e9854fb336c74b8370fc56fceb466f3b9f89cb04
nika1337 <nikanadaraia1337@gmail.com>
2025-02-21 15:49:36 +0400

    added Schedule Change Request detailed result

---
+++ Domain/Specification/ScheduleChangeRequest/Results/ScheduleChangeRequestDetailedResult.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Domain.Models;
+
+namespace Domain.Specification.ScheduleChangeRequest.Results;
+
+public record ScheduleChangeRequestDetailedResult
+{
+    public required int Id { get; init; }
+    public required string WorkerName { get; init; }
+    public required string JobName { get; init; }
+    public required DateOnly PreviousDate { get; init; }
+    public required PartOfDay PreviousPartOfDay {  get; init; }
+    public required DateOnly NewDate { get; init; }
+    public required PartOfDay NewPartOfDay { get; init; }
+    public required DateTime RequestDateTime { get; init; }
+}
