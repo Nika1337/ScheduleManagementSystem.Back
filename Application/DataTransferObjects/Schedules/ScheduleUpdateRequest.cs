ae98d286001f14a5c7937cc8247f22f07639a1f1
nika1337 <nikanadaraia1337@gmail.com>
2025-02-22 00:33:41 +0400

    Added dto for updating schedules

---
+++ Application/DataTransferObjects/Schedules/ScheduleUpdateRequest.cs
@@ -0,0 +1,12 @@
+﻿
+
+using Domain.Models;
+
+namespace Application.DataTransferObjects.Schedules;
+
+public record ScheduleUpdateRequest
+{
+    public Guid Id { get; init; }
+    public DateOnly Date { get; init; }
+    public PartOfDay PartOfDay { get; init; }
+}
