377fdcbbf30bc0f06d4337f9989eceb788f81f36
nika1337 <nikanadaraia1337@gmail.com>
2025-02-21 21:11:07 +0400

    added schedule detailed response

---
+++ Application/DataTransferObjects/Schedules/ScheduleDetailedResponse.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Domain.Models;
+
+namespace Application.DataTransferObjects.Schedules;
+
+public record ScheduleDetailedResponse
+{
+    public required Guid Id { get; init; }
+    public required string WorkerFirstName { get; init; }
+    public required string WorkerLastName { get; init; }
+    public required DateOnly Date { get; init; }
+    public required PartOfDay PartOfDay { get; init; }
+}
