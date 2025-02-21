0054e40bc2b83322992782cb1b202f50ab7a9455
nika1337 <nikanadaraia1337@gmail.com>
2025-02-21 16:38:34 +0400

    changed namespace ScheduleChangeRequest Specifications, also changed workedName to workerId, because worker related info will be stored in seperate identity database

--- Domain/Specification/ScheduleChangeRequests/Results/ScheduleChangeRequestDetailedResult.cs
+++ Domain/Specification/ScheduleChangeRequests/Results/ScheduleChangeRequestDetailedResult.cs
@@ -2,12 +2,12 @@
 
 using Domain.Models;
 
-namespace Domain.Specification.ScheduleChangeRequest.Results;
+namespace Domain.Specification.ScheduleChangeRequests.Results;
 
 public record ScheduleChangeRequestDetailedResult
 {
     public required int Id { get; init; }
-    public required string WorkerName { get; init; }
+    public required string WorkerId { get; init; }
     public required string JobName { get; init; }
     public required DateOnly PreviousDate { get; init; }
     public required PartOfDay PreviousPartOfDay {  get; init; }
