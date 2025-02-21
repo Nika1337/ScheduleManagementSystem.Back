47165bfc3d561db4ac3f71df316dc8faea0bfdbe
nika1337 <nikanadaraia1337@gmail.com>
2025-02-21 20:45:24 +0400

    added specification for detailed schedule

---
+++ Domain/Specification/Schedules/ScheduleDetailedSpecification.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Ardalis.Specification;
+using Domain.Models;
+using Domain.Specification.Schedules.Results;
+
+namespace Domain.Specification.Schedules;
+
+public class ScheduleDetailedSpecification : BaseModelByIdSpecification<Schedule, ScheduleDetailedResult>
+{
+    public ScheduleDetailedSpecification(Guid id) : base(id)
+    {
+        Query.Select(s => new ScheduleDetailedResult
+        {
+            Id = s.Id,
+            WorkerId = s.ScheduleOfWorkerId,
+            JobName = s.JobToPerform.Name,
+            Date = s.ScheduledAtDate,
+            PartOfDay = s.ScheduledAtPartOfDay
+        });
+    }
+}
