9b15dda780d1acaf2b0e883c37e2f0ec90699ff5
nika1337 <nikanadaraia1337@gmail.com>
2025-02-22 15:21:17 +0400

    added dto for schedule change request response

---
+++ Application/DataTransferObjects/ScheduleChangeRequests/ScheduleChangeRequestResponse.cs
@@ -0,0 +1,17 @@
+﻿
+using Domain.Models;
+
+namespace Application.DataTransferObjects.ScheduleChangeRequests;
+
+public record ScheduleChangeRequestResponse
+{
+    public required Guid Id { get; init; }
+    public required string WorkerFirstName { get; init; }
+    public required string WorkerLastName { get; init; }
+    public required string JobName { get; init; }
+    public required DateOnly PreviousDate { get; init; }
+    public required PartOfDay PreviousPartOfDay { get; init; }
+    public required DateOnly NewDate { get; init; }
+    public required PartOfDay NewPartOfDay { get; init; }
+    public required DateTime RequestDate { get; init; }
+}
