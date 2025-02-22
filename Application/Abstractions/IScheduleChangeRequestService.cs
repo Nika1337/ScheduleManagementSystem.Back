0bd7c021520b9a0f2d5593d66e2550af4ece1168
nika1337 <nikanadaraia1337@gmail.com>
2025-02-22 21:48:14 +0400

    added service for handling schedule change requests

---
+++ Application/Abstractions/IScheduleChangeRequestService.cs
@@ -0,0 +1,14 @@
+﻿
+using Application.DataTransferObjects.ScheduleChangeRequests;
+using Application.DataTransferObjects.Schedules;
+
+namespace Application.Abstractions;
+
+public interface IScheduleChangeRequestService
+{
+    Task<IEnumerable<ScheduleChangeRequestResponse>> GetScheduleChangeRequestsAsync();
+    Task RequestScheduleChangeAsync(ScheduleChangeRequest request);
+    Task AcceptScheduleChangeRequest(Guid id);
+    Task RejectScheduleChangeRequest(Guid id);
+    Task WithdrawScheduleChangeRequest(Guid id);
+}
--- Application/Abstractions/IScheduleService.cs
+++ Application/Abstractions/IScheduleService.cs
@@ -10,5 +10,4 @@ public interface IScheduleService
     Task<IEnumerable<ScheduleDetailedResponse>> GetSchedulesForWorkerAsync(Guid workerId, DateOnly startDate, DateOnly endDate);
     Task CreateScheduleAsync(ScheduleCreateRequest request);
     Task ChangeScheduleAsync(ScheduleChangeRequest request);
-    Task RequestScheduleChangeAsync(ScheduleChangeRequest request);
 }
