acddda146d8a2f14500fa0754ee11c59c2bf56bc
nika1337 <nikanadaraia1337@gmail.com>
2025-02-22 16:01:16 +0400

    added dto for updating employees profile

---
+++ Application/DataTransferObjects/Employees/EmployeeProfileUpdateRequest.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Application.DataTransferObjects.Employees;
+
+public record EmployeeProfileUpdateRequest
+{
+    public required Guid Id { get; init; }
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required string Email { get; init; }
+    public string? ProfilePictureBase64 { get; init; }
+}
