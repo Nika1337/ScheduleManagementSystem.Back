a607ab13cdac0f41e3c9488f1c51187e1ab903fa
nika1337 <nikanadaraia1337@gmail.com>
2025-02-22 17:48:30 +0400

    added service for working with schedules

---
+++ Application/Abstractions/IScheduleService.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Application.DataTransferObjects.Schedules;
+
+namespace Application.Abstractions;
+
+public interface IScheduleService
+{
+    Task<IEnumerable<ScheduleDetailedResponse>> GetSchedulesAsync(DateOnly startDate, DateOnly endDate);
+    Task<IEnumerable<ScheduleDetailedResponse>> GetSchedulesForWorkerAsync(Guid workerId, DateOnly firstDate, DateOnly endDate);
+    Task CreateScheduleAsync(ScheduleCreateRequest request);
+    Task ChangeScheduleAsync(ScheduleChangeRequest request);
+    Task RequestScheduleChange(ScheduleChangeRequest request);
+}
