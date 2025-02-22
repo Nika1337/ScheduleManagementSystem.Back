b2b806321084263c5d47571d6d3f180e9681022d
nika1337 <nikanadaraia1337@gmail.com>
2025-02-22 15:35:25 +0400

    added dto fro creating employees

---
+++ Application/DataTransferObjects/Employees/EmployeeCreateRequest.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Application.DataTransferObjects.Employees;
+
+public record EmployeeCreateRequest
+{
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required string Email { get; init; }
+    public required string RoleName { get; init; }
+}
