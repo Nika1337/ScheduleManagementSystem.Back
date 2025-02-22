53786b51db9a1e1acf09a0060cc957cf2bc0439d
nika1337 <nikanadaraia1337@gmail.com>
2025-02-22 15:38:19 +0400

    added dto for updating employee by admin

---
+++ Application/DataTransferObjects/Employees/EmployeeUpdateByAdminRequest.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace Application.DataTransferObjects.Employees;
+
+public record EmployeeUpdateByAdminRequest
+{
+    public required Guid Id { get; init; }
+    public required string RoleName { get; init; }
+}
