ee6ca66cffcf4e1c7733825c3c5c7b8d46ad2e1c
nika1337 <nikanadaraia1337@gmail.com>
2025-02-22 17:50:01 +0400

    rename method of schedule service

--- Application/Abstractions/IScheduleService.cs
+++ Application/Abstractions/IScheduleService.cs
@@ -7,8 +7,8 @@ namespace Application.Abstractions;
 public interface IScheduleService
 {
     Task<IEnumerable<ScheduleDetailedResponse>> GetSchedulesAsync(DateOnly startDate, DateOnly endDate);
-    Task<IEnumerable<ScheduleDetailedResponse>> GetSchedulesForWorkerAsync(Guid workerId, DateOnly firstDate, DateOnly endDate);
+    Task<IEnumerable<ScheduleDetailedResponse>> GetSchedulesForWorkerAsync(Guid workerId, DateOnly startDate, DateOnly endDate);
     Task CreateScheduleAsync(ScheduleCreateRequest request);
     Task ChangeScheduleAsync(ScheduleChangeRequest request);
-    Task RequestScheduleChange(ScheduleChangeRequest request);
+    Task RequestScheduleChangeAsync(ScheduleChangeRequest request);
 }
