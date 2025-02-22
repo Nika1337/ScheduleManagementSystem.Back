9ed61ba51beee728595f2576c7c220aebf2637df
nika1337 <nikanadaraia1337@gmail.com>
2025-02-22 15:33:16 +0400

    added dto for schedule change request create request

---
+++ Application/DataTransferObjects/ScheduleChangeRequests/ScheduleChangeRequestCreateRequest.cs
@@ -0,0 +1,12 @@
+﻿
+
+using Domain.Models;
+
+namespace Application.DataTransferObjects.ScheduleChangeRequests;
+
+public record ScheduleChangeRequestCreateRequest
+{
+    public required Guid ScheduleId { get; init; }
+    public required DateOnly NewDate { get; init; }
+    public required PartOfDay PartOfDay { get; init; }
+}
