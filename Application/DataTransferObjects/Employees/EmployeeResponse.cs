6306df2909f4d7a6e64b210a6cba038cd557ee17
nika1337 <nikanadaraia1337@gmail.com>
2025-02-22 16:51:59 +0400

    added response dto for employee

---
+++ Application/DataTransferObjects/Employees/EmployeeResponse.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Application.DataTransferObjects.Employees;
+
+public record EmployeeResponse
+{
+    public required Guid Id { get; init; }
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required string Email { get; init; }
+    public required string StartDate { get; init; }
+    public required string RoleName { get; init; }
+}
