e753a6fd1fcf533320a9ee8c7ca4f6bb0f06c3d1
nika1337 <nikanadaraia1337@gmail.com>
2025-02-23 16:39:35 +0400

    added configuration for job

---
+++ Infrastructure/Data/Config/JobConfiguration.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config;
+
+internal class JobConfiguration : IEntityTypeConfiguration<Job>
+{
+    public void Configure(EntityTypeBuilder<Job> builder)
+    {
+        builder
+            .HasKey(job => job.Id);
+
+        builder
+            .Property(job => job.Name)
+            .HasMaxLength(80);
+
+    }
+}
