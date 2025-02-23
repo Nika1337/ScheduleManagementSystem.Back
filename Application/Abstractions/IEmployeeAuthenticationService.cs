5db8642309e9dfe92e0f364bd2b0a8422976fa08
nika1337 <nikanadaraia1337@gmail.com>
2025-02-23 16:10:27 +0400

    added service for authenticating employees

---
+++ Application/Abstractions/IEmployeeAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿
+
+using System.Security.Claims;
+
+namespace Application.Abstractions;
+
+public interface IEmployeeAuthenticationService
+{
+    Task<string> PasswordSignInAsync(string id, string password);
+    Task ChangePasswordAsync(ClaimsPrincipal claimsPrincipal, string currentPassword, string newPassword);
+}
