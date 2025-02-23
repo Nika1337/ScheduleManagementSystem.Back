ef7281a77b71b1d2e2f2ae29a4a269bc553a483f
nika1337 <nikanadaraia1337@gmail.com>
2025-02-23 17:07:32 +0400

    added ef core repository implementation of IRepository

---
+++ Infrastructure/Data/EfRepository.cs
@@ -0,0 +1,9 @@
+﻿using Ardalis.Specification.EntityFrameworkCore;
+using Domain.Models;
+using Domain.Abstractions;
+
+namespace Infrastructure.Data;
+
+internal class EfRepository<T>(ScheduleContext dbContext) : RepositoryBase<T>(dbContext), IRepository<T> where T : BaseModel
+{
+}
