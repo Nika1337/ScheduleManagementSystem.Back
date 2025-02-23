dc06ed4c208a7565471918ac50cea0e462e8263e
nika1337 <nikanadaraia1337@gmail.com>
2025-02-23 17:00:15 +0400

    added context for schedules

---
+++ Infrastructure/Data/ScheduleContext.cs
@@ -0,0 +1,22 @@
+﻿
+using Domain.Models;
+using Infrastructure.Data.Config;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+internal class ScheduleContext(DbContextOptions<ScheduleContext> options) : DbContext(options)
+{
+    public DbSet<Job> Jobs { get; set; }
+    public DbSet<Schedule> Schedules { get; set; }
+    public DbSet<ScheduleChangeRequest> ScheduleChangeRequests { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.ApplyConfiguration(new JobConfiguration());
+        builder.ApplyConfiguration(new ScheduleConfiguration());
+        builder.ApplyConfiguration(new ScheduleChangeRequestConfiguration());
+    }
+}
