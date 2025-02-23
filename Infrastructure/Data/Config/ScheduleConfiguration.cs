633c51db8b82d746c714864c000d6970136585c8
nika1337 <nikanadaraia1337@gmail.com>
2025-02-23 16:55:15 +0400

    added configuration for schedules

---
+++ Infrastructure/Data/Config/ScheduleConfiguration.cs
@@ -0,0 +1,33 @@
+﻿
+
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config;
+
+internal class ScheduleConfiguration : IEntityTypeConfiguration<Schedule>
+{
+    public void Configure(EntityTypeBuilder<Schedule> builder)
+    {
+        builder
+            .HasKey(sch => sch.Id);
+
+        builder
+            .HasOne(sch => sch.JobToPerform)
+            .WithMany();
+
+        builder
+            .HasOne(sch => sch.ScheduleChangeRequest)
+            .WithOne(sch => sch.ScheduleToChange);
+
+        builder
+            .HasIndex(sch => sch.ScheduleOfWorkerId);
+
+        builder
+            .HasIndex(sch => sch.ScheduledAtDate);
+
+        builder
+            .HasIndex(sch => new { sch.ScheduleOfWorkerId, sch.ScheduledAtDate });
+    }
+}
