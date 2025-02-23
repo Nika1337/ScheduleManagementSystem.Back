7a376e2882709b35bed391ac69ea0113c0fba88d
nika1337 <nikanadaraia1337@gmail.com>
2025-02-23 16:57:28 +0400

    added configuration fo schedule change requests

---
+++ Infrastructure/Data/Config/ScheduleChangeRequestConfiguration.cs
@@ -0,0 +1,18 @@
+﻿
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config;
+
+internal class ScheduleChangeRequestConfiguration : IEntityTypeConfiguration<ScheduleChangeRequest>
+{
+    public void Configure(EntityTypeBuilder<ScheduleChangeRequest> builder)
+    {
+        builder
+            .HasKey(sch => sch.Id);
+
+        builder
+            .HasIndex(sch => sch.RequestDateTime);
+    }
+}
