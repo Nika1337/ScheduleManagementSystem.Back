e68eb188b6640e4b9f777ca051e35e345efb4c0c
nika1337 <nikanadaraia1337@gmail.com>
2025-02-23 16:05:44 +0400

    added service for employee data

---
+++ Application/Abstractions/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Application.DataTransferObjects.Employees;
+
+namespace Application.Abstractions;
+
+public interface IEmployeeService
+{
+    Task CreateEmployeeAsync(EmployeeCreateRequest request);
+    Task<IEnumerable<EmployeeResponse>> GetEmployeesAsync();
+    Task UpdateEmployeeAsync(EmployeeUpdateByAdminRequest request);
+    Task UpdateEmployeeAsync(EmployeeProfileUpdateRequest request);
+}
