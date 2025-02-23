5c5df5a30f46dd06268b02778db41e4df1d128cf
nika1337 <nikanadaraia1337@gmail.com>
2025-02-23 17:33:34 +0400

    added identity employee

---
+++ Infrastructure/Identity/Entities/IdentityEmployee.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity.Entities;
+
+public class IdentityEmployee : IdentityUser<Guid>
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+
+}
