73bc75c4f902f567615167e955289098e107a137
nika1337 <nikanadaraia1337@gmail.com>
2025-02-26 03:13:31 +0400

    implemented job service

---
+++ Infrastructure/Services/JobService.cs
@@ -0,0 +1,41 @@
+﻿
+
+using Application.Abstractions;
+using Application.DataTransferObjects.Jobs;
+using Domain.Abstractions;
+using Domain.Models;
+
+namespace Infrastructure.Services;
+
+internal class JobService : IJobService
+{
+    private readonly IRepository<Job> _repository;
+
+    public JobService(IRepository<Job> jobRepository)
+    {
+        _repository = jobRepository;
+    }
+
+    public async Task CreateJobAsync(JobCreateRequest request)
+    {
+        var job = new Job
+        {
+            Name = request.Name,
+        };
+
+        await _repository.AddAsync(job);
+    }
+
+    public async Task<IEnumerable<JobResponse>> GetJobsAsync()
+    {
+        var jobs = await _repository.ListAsync();
+
+        var response = jobs.Select(job => new JobResponse
+        {
+            Id = job.Id,
+            Name = job.Name,
+        });
+
+        return response;
+    }
+}
