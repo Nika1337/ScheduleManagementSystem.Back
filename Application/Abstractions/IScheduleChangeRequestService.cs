854445f9fbf42e6cca23048962589c697b747739
nika1337 <nikanadaraia1337@gmail.com>
2025-02-26 03:42:01 +0400

    moved method to request schedule change from schedule change request service to schedule service

--- Application/Abstractions/IScheduleChangeRequestService.cs
+++ Application/Abstractions/IScheduleChangeRequestService.cs
@@ -1,13 +1,11 @@
 ﻿
 using Application.DataTransferObjects.ScheduleChangeRequests;
-using Application.DataTransferObjects.Schedules;
 
 namespace Application.Abstractions;
 
 public interface IScheduleChangeRequestService
 {
     Task<IEnumerable<ScheduleChangeRequestResponse>> GetScheduleChangeRequestsAsync();
-    Task RequestScheduleChangeAsync(ScheduleChangeRequest request);
     Task AcceptScheduleChangeRequest(Guid id);
     Task RejectScheduleChangeRequest(Guid id);
     Task WithdrawScheduleChangeRequest(Guid id);
