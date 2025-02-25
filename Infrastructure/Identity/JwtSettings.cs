8616520d9a00997f269c6d6558b020e0f27c8485
nika1337 <nikanadaraia1337@gmail.com>
2025-02-25 14:43:30 +0400

    added jwt settings class

---
+++ Infrastructure/Identity/JwtSettings.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace Infrastructure.Identity;
+
+public class JwtSettings
+{
+    public string Secret { get; private init; }
+    public string Issuer { get; private init; }
+    public string Audience { get; private init; }
+    public int ExpiryMinutes { get; private init; }
+
+    public JwtSettings(
+        string secret = "", 
+        string issuer = "", 
+        string audience = "",
+        int expiryMinutes = 120)
+    {
+        Secret = secret;
+        Issuer = issuer;
+        Audience = audience;
+        ExpiryMinutes = expiryMinutes;
+    }
+}
