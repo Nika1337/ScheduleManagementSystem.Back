6b8270afb47d4f46975a578efe888376eb85ad5f
nika1337 <nikanadaraia1337@gmail.com>
2025-02-25 11:31:26 +0400

    added exception for incorrect password

---
+++ Domain/Exceptions/PasswordIncorrectException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Domain.Exceptions;
+
+
+public class PasswordIncorrectException : Exception
+{
+    public PasswordIncorrectException(Guid id) : base($"Password for user with id '{id}' is incorrect")
+    {
+
+    }
+}
