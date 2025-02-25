522ac50c86aff0ab031a5366b2352a51ca08a3c4
nika1337 <nikanadaraia1337@gmail.com>
2025-02-26 03:22:44 +0400

    added specification to get schedule change requests with respective schedule objects

---
+++ Domain/Specification/ScheduleChangeRequests/ScheduleChangeRequestWithScheduleToChangeByIdSpecification.cs
@@ -0,0 +1,15 @@
+﻿
+using Ardalis.Specification;
+using Domain.Models;
+
+namespace Domain.Specification.ScheduleChangeRequests;
+
+internal class ScheduleChangeRequestWithScheduleToChangeByIdSpecification : SingleResultSpecification<ScheduleChangeRequest>
+{
+    public ScheduleChangeRequestWithScheduleToChangeByIdSpecification(Guid id)
+    {
+        Query.Where(scr => scr.Id == id);
+
+        Query.Include(scr => scr.ScheduleToChange);
+    }
+}
