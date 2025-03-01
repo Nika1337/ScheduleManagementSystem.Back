61c85b01eda2ee38d4f47f42ae5cb72e3d06cc48
nika1337 <nikanadaraia1337@gmail.com>
2025-03-01 16:48:25 +0400

    change EmployeeAuthenticationService interface and implementation to return EmployeeAuthenticationResponse after successful authentication

--- Application/Abstractions/IEmployeeAuthenticationService.cs
+++ Application/Abstractions/IEmployeeAuthenticationService.cs
@@ -1,10 +1,12 @@
 ﻿
 
 
+using Application.DataTransferObjects.Employees;
+
 namespace Application.Abstractions;
 
 public interface IEmployeeAuthenticationService
 {
-    Task<string> PasswordSignInAsync(string email, string password);
+    Task<EmployeeAuthenticationResponse> AuthenticateAsync(string email, string password);
     Task ChangePasswordAsync(Guid userId, string currentPassword, string newPassword);
 }
--- Infrastructure/Services/IdentityEmployeeAuthenticationService.cs
+++ Infrastructure/Services/IdentityEmployeeAuthenticationService.cs
@@ -1,39 +1,38 @@
 ﻿using Application.Abstractions;
+using Application.DataTransferObjects.Employees;
 using Domain.Exceptions;
-using Infrastructure.Identity;
 using Infrastructure.Identity.Entities;
 using Infrastructure.Identity.Validators;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
 
 namespace Infrastructure.Services;
 
 internal class IdentityEmployeeAuthenticationService : IEmployeeAuthenticationService
 {
     private readonly UserManager<IdentityEmployee> _userManager;
-    private readonly JwtSettings _jwtSettings;
-    private readonly JwtSecurityTokenHandler _tokenHandler = new();
 
     public IdentityEmployeeAuthenticationService(
-        UserManager<IdentityEmployee> userManager,
-        IOptions<JwtSettings> jwtSettings)
+        UserManager<IdentityEmployee> userManager)
     {
         _userManager = userManager;
-        _jwtSettings = jwtSettings.Value ?? throw new ArgumentNullException(nameof(jwtSettings));
     }
 
-    public async Task<string> PasswordSignInAsync(string email, string password)
+    public async Task<EmployeeAuthenticationResponse> AuthenticateAsync(string email, string password)
     {
         var user = await _userManager.FindByEmailAsync(email) ?? throw new NotFoundException($"User with email '{email}' not found.");
 
         if (!await _userManager.CheckPasswordAsync(user, password))
             throw new PasswordIncorrectException();
 
-        return await GenerateJwtTokenAsync(user);
+        var userRoleName = (await _userManager.GetRolesAsync(user)).First();
+
+        var response = new EmployeeAuthenticationResponse
+        {
+            EmployeeId = user.Id,
+            RoleName = userRoleName,
+        };
+
+        return response;
     }
 
     public async Task ChangePasswordAsync(Guid userId, string currentPassword, string newPassword)
@@ -60,28 +59,4 @@ internal class IdentityEmployeeAuthenticationService : IEmployeeAuthenticationSe
         }
     }
 
-    private async Task<string> GenerateJwtTokenAsync(IdentityEmployee user)
-    {
-        var roles = await _userManager.GetRolesAsync(user);
-
-        var claims = new List<Claim>
-        {
-            new(ClaimTypes.NameIdentifier, user.Id.ToString()),
-            new(ClaimTypes.Name, $"{user.FirstName} {user.LastName}"),
-            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
-        };
-        claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
-
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Secret));
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-
-        var token = new JwtSecurityToken(
-            issuer: _jwtSettings.Issuer,
-            audience: _jwtSettings.Audience,
-            claims: claims,
-            expires: DateTime.UtcNow.AddMinutes(_jwtSettings.ExpiryMinutes),
-            signingCredentials: creds);
-
-        return _tokenHandler.WriteToken(token);
-    }
 }
