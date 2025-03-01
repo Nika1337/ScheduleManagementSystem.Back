68ce2c166d24286f6faae83051536c545993d65e
nika1337 <nikanadaraia1337@gmail.com>
2025-03-01 23:53:02 +0400

    added endpoint for updating profile

---
+++ Presentation/Features/Profile/UpdateProfile/Endpoint.cs
@@ -0,0 +1,38 @@
+﻿using Application.Abstractions;
+using Application.DataTransferObjects.Employees;
+using System.Security.Claims;
+
+namespace Profile.UpdateProfile;
+
+internal sealed class Endpoint : Endpoint<Request>
+{
+    private readonly IEmployeeService _employeeService;
+
+    public Endpoint(IEmployeeService employeeService)
+    {
+        _employeeService = employeeService;
+    }
+
+    public override void Configure()
+    {
+        Patch("/profile");
+    }
+
+    public override async Task HandleAsync(Request r, CancellationToken c)
+    {
+        var employeeIdClaim = User.FindFirst(ClaimTypes.NameIdentifier) ?? throw new UnauthorizedAccessException();
+        var employeeId = Guid.Parse(employeeIdClaim.Value);
+
+        var updateRequest = new EmployeeProfileUpdateRequest 
+        {
+            Id = employeeId,
+            FirstName = r.FirstName,
+            LastName = r.LastName,
+            Email = r.Email
+        };
+
+
+        await _employeeService.UpdateEmployeeAsync(updateRequest);
+    }
+
+}
---
+++ Presentation/Features/Profile/UpdateProfile/Models.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace Profile.UpdateProfile;
+
+internal sealed class Request
+{
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required string Email { get; init; }
+
+    internal sealed class Validator : Validator<Request>
+    {
+        public Validator()
+        {
+            RuleFor(r => r.FirstName)
+                .Length(1, 250);
+
+            RuleFor(r => r.LastName)
+                .Length(1, 250);
+
+            RuleFor(r => r.Email)
+                .Length(1, 250)
+                .EmailAddress();
+        }
+    }
+}
