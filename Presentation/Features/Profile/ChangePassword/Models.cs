6e0cfd3805045bae38148dc6912318221bdec5cd
nika1337 <nikanadaraia1337@gmail.com>
2025-03-02 00:19:20 +0400

    implemented change password endpoint

---
+++ Presentation/Features/Profile/ChangePassword/Models.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+
+namespace Profile.ChangePassword;
+
+internal sealed class Request
+{
+    public required string CurrentPassword { get; init; }
+    public required string NewPassword { get; init; }
+
+    internal sealed class Validator : Validator<Request>
+    {
+        public Validator()
+        {
+            RuleFor(r => r.CurrentPassword)
+                .NotEmpty().WithMessage("Current password is required.")
+                .Length(8, 64).WithMessage("Current password must be between 8 and 64 characters.");
+
+            RuleFor(r => r.NewPassword)
+                .NotEmpty().WithMessage("New password is required.")
+                .Length(8, 64).WithMessage("New password must be between 8 and 64 characters.")
+                .NotEqual(r => r.CurrentPassword).WithMessage("New password must be different from the current password.");
+
+        }
+    }
+}
