a93d4eebc2fd12a301bab577aba628921c91f096
nika1337 <nikanadaraia1337@gmail.com>
2025-03-01 16:44:33 +0400

    added employee authentication response

---
+++ Application/DataTransferObjects/Employees/EmployeeAuthenticationResponse.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace Application.DataTransferObjects.Employees;
+public record EmployeeAuthenticationResponse
+{
+    public required Guid EmployeeId { get; init; }
+    public required string RoleName { get; init; }
+}
