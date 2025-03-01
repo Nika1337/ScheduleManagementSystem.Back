b5b6c37ef7a619b03e6d28e513588c6dec2c549d
nika1337 <nikanadaraia1337@gmail.com>
2025-03-01 18:00:00 +0400

    added response object for employees' profile

---
+++ Application/DataTransferObjects/Employees/EmployeeProfileResponse.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace Application.DataTransferObjects.Employees;
+public record EmployeeProfileResponse
+{
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required string Email { get; init; }
+}
