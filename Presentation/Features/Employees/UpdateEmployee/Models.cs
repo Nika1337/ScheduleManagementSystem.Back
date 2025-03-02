64946458a3d448edafee78909693f47c70cd047d
nika1337 <nikanadaraia1337@gmail.com>
2025-03-02 22:22:59 +0400

    added endpoint for updating employee

---
+++ Presentation/Features/Employees/UpdateEmployee/Models.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employees.UpdateEmployee;
+
+internal sealed class Request
+{
+    [FromRoute]
+    public required Guid EmployeeId { get; init; }
+    public required string RoleName { get; init; }
+
+    internal sealed class Validator : Validator<Request>
+    {
+        public Validator()
+        {
+            RuleFor(r => r.EmployeeId)
+                .NotEmpty();
+
+            RuleFor(r => r.RoleName)
+                .NotEmpty();
+        }
+    }
+}
