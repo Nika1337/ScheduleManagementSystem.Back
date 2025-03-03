fd89d6c096c7413423cc5dead43fa8bd151d1ed5
nika1337 <nikanadaraia1337@gmail.com>
2025-03-04 00:38:20 +0400

    added endpoint for changing schedule

---
+++ Presentation/Features/Schedules/ChangeSchedule/Endpoint.cs
@@ -0,0 +1,57 @@
+﻿using Application.Abstractions;
+using Application.DataTransferObjects.Schedules;
+using System.Security.Claims;
+
+namespace Schedules.ChangeSchedule;
+
+internal sealed class Endpoint : Endpoint<Request>
+{
+    private readonly IScheduleService _scheduleService;
+
+    public Endpoint(IScheduleService scheduleService)
+    {
+        _scheduleService = scheduleService;
+    }
+
+    public override void Configure()
+    {
+        Patch("/schedules/{Id}");
+    }
+
+    public override async Task HandleAsync(Request r, CancellationToken c)
+    {
+        var userRole = User.FindFirstValue(ClaimTypes.Role);
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+
+        if (userRole == "Admin")
+        {
+            var request = new ScheduleChangeRequest
+            {
+                Id = r.Id,
+                Date = r.Date,
+                PartOfDay = r.PartOfDay
+            };
+            await _scheduleService.ChangeScheduleAsync(request);
+        }
+        else if (userRole == "Worker" && Guid.TryParse(userId, out var workerId))
+        {
+            var request = new ScheduleChangeByWorkerRequest
+            {
+                WorkerId = workerId,
+                Id = r.Id,
+                Date = r.Date,
+                PartOfDay = r.PartOfDay
+            };
+            await _scheduleService.RequestScheduleChangeAsync(request);
+        }
+        else
+        {
+            await SendForbiddenAsync();
+            return;
+        }
+
+
+        await SendNoContentAsync();
+    }
+}
---
+++ Presentation/Features/Schedules/ChangeSchedule/Models.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Models;
+using FluentValidation;
+
+namespace Schedules.ChangeSchedule;
+
+internal sealed class Request
+{
+    public required Guid Id { get; init; }
+    public required DateOnly Date {  get; init; }
+    public required PartOfDay PartOfDay { get; init; }
+
+    internal sealed class Validator : Validator<Request>
+    {
+        public Validator()
+        {
+            RuleFor(r => r.Id)
+                .NotEmpty();
+
+            RuleFor(r => r.Date)
+                .NotEmpty()
+                .GreaterThan(DateOnly.FromDateTime(DateTime.Now.AddDays(-1)));
+
+            RuleFor(r => r.PartOfDay)
+                .NotEmpty();
+        }
+    }
+}
