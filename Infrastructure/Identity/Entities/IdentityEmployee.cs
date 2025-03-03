cb1b0657bf1dbc6e5a9beeb1183e93e6a6020549
nika1337 <nikanadaraia1337@gmail.com>
2025-03-04 00:54:50 +0400

    changed identityEmployees visibility to internal

--- Infrastructure/Identity/Entities/IdentityEmployee.cs
+++ Infrastructure/Identity/Entities/IdentityEmployee.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Infrastructure.Identity.Entities;
 
-public class IdentityEmployee : IdentityUser<Guid>
+internal class IdentityEmployee : IdentityUser<Guid>
 {
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
