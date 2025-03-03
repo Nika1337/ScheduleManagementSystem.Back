716f296c464081b80e219c3f6e66fad9baf45415
nika1337 <nikanadaraia1337@gmail.com>
2025-03-04 01:15:10 +0400

    added class for registering dependencies of presentation layer

---
+++ Presentation/Dependencies.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Presentation;
+public static class Dependencies
+{
+    public static IServiceCollection AddPresentationLayer(this IServiceCollection services)
+    {
+        services.AddFastEndpoints();
+        return services;
+    }
+
+    public static IApplicationBuilder UsePresentationLayer(this IApplicationBuilder app)
+    {
+        app.UseFastEndpoints();
+        app.UseSwaggerGen();
+        return app;
+    }
+}
