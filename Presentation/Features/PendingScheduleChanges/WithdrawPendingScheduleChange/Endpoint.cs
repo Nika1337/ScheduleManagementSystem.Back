574e83d55882762d3bd4bc5814ce54db59a64f2b
nika1337 <nikanadaraia1337@gmail.com>
2025-03-03 21:46:45 +0400

    added endpoint for withdrawing pending schedule changes

---
+++ Presentation/Features/PendingScheduleChanges/WithdrawPendingScheduleChange/Endpoint.cs
@@ -0,0 +1,31 @@
+﻿using Application.Abstractions;
+using System.Security.Claims;
+
+namespace PendingScheduleChange.WithdrawPendingScheduleChange;
+
+internal sealed class Endpoint : Endpoint<Request>
+{
+
+    private readonly IPendingScheduleChangeService _pendingScheduleChangeService;
+
+    public Endpoint(IPendingScheduleChangeService pendingScheduleChangeService)
+    {
+        _pendingScheduleChangeService = pendingScheduleChangeService;
+    }
+
+    public override void Configure()
+    {
+        Post("/pending-schedule-changes/{Id}/withdraw");
+        Roles("Worker");
+    }
+
+    public override async Task HandleAsync(Request r, CancellationToken c)
+    {
+        var employeeIdClaim = User.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new UnauthorizedAccessException();
+        var employeeId = Guid.Parse(employeeIdClaim);
+
+        await _pendingScheduleChangeService.WithdrawPendingScheduleChange(r.Id, employeeId);
+
+        await SendNoContentAsync();
+    }
+}
---
+++ Presentation/Features/PendingScheduleChanges/WithdrawPendingScheduleChange/Models.cs
@@ -0,0 +1,13 @@
+﻿namespace PendingScheduleChange.WithdrawPendingScheduleChange;
+
+internal sealed class Request
+{
+    public required Guid Id { get; init; }
+    internal sealed class Validator : Validator<Request>
+    {
+        public Validator()
+        {
+
+        }
+    }
+}
