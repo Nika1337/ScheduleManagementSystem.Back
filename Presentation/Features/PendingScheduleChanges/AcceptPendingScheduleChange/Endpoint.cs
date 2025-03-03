3c5e4dc002a5fb0511bfa43420ae2bb938fa7d3c
nika1337 <nikanadaraia1337@gmail.com>
2025-03-03 21:11:01 +0400

    implemented endpoint for accepting pending schedule changes

---
+++ Presentation/Features/PendingScheduleChanges/AcceptPendingScheduleChange/Endpoint.cs
@@ -0,0 +1,26 @@
+﻿using Application.Abstractions;
+
+namespace PendingScheduleChanges.AcceptPendingScheduleChange;
+
+internal sealed class Endpoint : Endpoint<Request>
+{
+    private readonly IPendingScheduleChangeService _pendingScheduleChangeService;
+
+    public Endpoint(IPendingScheduleChangeService pendingScheduleChangeService)
+    {
+        _pendingScheduleChangeService = pendingScheduleChangeService;
+    }
+
+    public override void Configure()
+    {
+        Post("/pending-schedule-changes/{Id}/accept");
+        Roles("Admin");
+    }
+
+    public override async Task HandleAsync(Request r, CancellationToken c)
+    {
+        await _pendingScheduleChangeService.AcceptPendingScheduleChange(r.Id);
+
+        await SendNoContentAsync(c);
+    }
+}
---
+++ Presentation/Features/PendingScheduleChanges/AcceptPendingScheduleChange/Models.cs
@@ -0,0 +1,19 @@
+﻿
+
+using FluentValidation;
+
+namespace PendingScheduleChanges.AcceptPendingScheduleChange;
+
+internal sealed class Request
+{
+    public required Guid Id { get; init; }
+
+    internal sealed class Validator : Validator<Request>
+    {
+        public Validator()
+        {
+            RuleFor(r => r.Id)
+                .NotEmpty();
+        }
+    }
+}
