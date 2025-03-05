486e58cd22841302c9acacad047ccbfa45630b82
nika1337 <nikanadaraia1337@gmail.com>
2025-03-05 06:59:40 +0400

    added interface and implementation of schedule notification service

---
+++ Application/Abstractions/IScheduleNotificationService.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Application.DataTransferObjects.Schedules;
+
+namespace Application.Abstractions;
+public interface IScheduleNotificationService
+{
+    IAsyncEnumerable<ScheduleChangeEvent> SubscribeWorkerChanges(Guid workerId, CancellationToken cancellationToken);
+
+    ValueTask PublishChangeAsync(ScheduleChangeEvent scheduleChangeEvent, CancellationToken cancellationToken);
+}
---
+++ Infrastructure/Services/ScheduleNotificationService.cs
@@ -0,0 +1,40 @@
+﻿using Application.Abstractions;
+using Application.DataTransferObjects.Schedules;
+using System.Collections.Concurrent;
+using System.Threading.Channels;
+
+namespace Infrastructure.Services;
+internal class ScheduleNotificationService : IScheduleNotificationService
+{
+    private readonly ConcurrentDictionary<Guid, Channel<ScheduleChangeEvent>> _channels = new();
+
+    private Channel<ScheduleChangeEvent> GetOrCreateChannel(Guid workerId)
+    {
+        return _channels.GetOrAdd(workerId, _ =>
+        {
+            return Channel.CreateUnbounded<ScheduleChangeEvent>(
+                new UnboundedChannelOptions
+                {
+                    SingleReader = false,
+                    SingleWriter = false
+                });
+        });
+    }
+
+    public IAsyncEnumerable<ScheduleChangeEvent> SubscribeWorkerChanges(
+        Guid userId,
+        CancellationToken cancellationToken)
+    {
+        var channel = GetOrCreateChannel(userId);
+        return channel.Reader.ReadAllAsync(cancellationToken);
+    }
+
+    public async ValueTask PublishChangeAsync(
+        ScheduleChangeEvent ev,
+        CancellationToken cancellationToken)
+    {
+
+        var channel = GetOrCreateChannel(ev.WorkerId);
+        await channel.Writer.WriteAsync(ev, cancellationToken);
+    }
+}
