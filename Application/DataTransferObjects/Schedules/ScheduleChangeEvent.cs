fbb6968f4f3463882cebe3b460fcab6f4e57ccf4
nika1337 <nikanadaraia1337@gmail.com>
2025-03-05 06:50:53 +0400

    added event for schedule change

---
+++ Application/DataTransferObjects/Schedules/ScheduleChangeEvent.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Application.DataTransferObjects.Schedules;
+public record ScheduleChangeEvent
+{
+    public required Guid WorkerId { get; init; }
+    public required string Message { get; init; }
+}
