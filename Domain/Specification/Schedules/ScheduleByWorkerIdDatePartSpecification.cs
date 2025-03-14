856e7800a3fdf45a6339b5d806a930dbee115b5b
nika1337 <nikanadaraia1337@gmail.com>
2025-03-14 12:11:51 +0400

    added specification for getting schedules by worker id, date, part of day

---
+++ Domain/Specification/Schedules/ScheduleByWorkerIdDatePartSpecification.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Ardalis.Specification;
+using Domain.Models;
+
+namespace Domain.Specification.Schedules;
+public class ScheduleByWorkerIdDatePartSpecification : SingleResultSpecification<Schedule>
+{
+    public ScheduleByWorkerIdDatePartSpecification(Guid workerId, DateOnly date, PartOfDay partOfDay) 
+    {
+        Query.Where(schedule => 
+            schedule.ScheduleOfWorker.Id == workerId && 
+            schedule.ScheduledAtDate == date &&
+            schedule.ScheduledAtPartOfDay == partOfDay);
+    }
+}
